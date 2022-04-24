616ea9cbbf5b85d41e3b38e98a0459d120e4d00b
Oliver Cardoza <me@olivercardoza.com>
2022-04-23 23:28:02 -0400

    Update deploy script and ignore terraform cache files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 PROJECT_ID=domo-334121
-CLOUD_REGION=northamerica-northeast2
+CLOUD_REGION=northamerica-northeast1
 GAR_REGISTRY=${CLOUD_REGION}-docker.pkg.dev
 GAR_REPO=${GAR_REGISTRY}/${PROJECT_ID}/domo
-RUN_SERVICE_NAME=bot-8dd53b271efab276131aa26d27e1e4a9
+RUN_SERVICE_NAME=domo-run
 
 # Required by ko
 export KO_DOCKER_REPO=northamerica-northeast2-docker.pkg.dev/domo-334121/domo
@@ -16,4 +16,8 @@ gcloud auth configure-docker ${GCP_REGION} --project ${PROJECT_ID} --verbosity e
 
 # Build and deploy container to artifact registry
 image_ref=$(ko publish ./cmd/bot/)
-gcloud run deploy ${RUN_SERVICE_NAME} --image=${image_ref} --region ${CLOUD_REGION}
+gcloud run deploy ${RUN_SERVICE_NAME} \
+  --image=${image_ref} \
+  --region ${CLOUD_REGION} \
+  --set-secrets=/config/bot_config=bot_config:latest,DISCORD_TOKEN=discord_token:latest \
+  --args=-c,/config/bot_config
